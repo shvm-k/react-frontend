b9056fdfcf718c5ed71913065266b55f6e6aaa15
Nilabh Shivam Thakur <96217047+shvm-k@users.noreply.github.com>
2023-09-02 10:48:38 +0530

    Add files via upload

---
+++ mern-blog/client/src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div className='mb-20'>
+    <h1 className='sm:text-4xl text-2xl font-blod my-6 text-gray-900'>About</h1>
+    <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis enim rhoncus ligula pulvinar bibendum. Aenean interdum arcu in nisi placerat feugiat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur vitae elit non nulla auctor mollis vel at elit. Morbi tincidunt nibh non ligula. </p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce quis enim rhoncus ligula pulvinar bibendum. Aenean interdum arcu in nisi placerat feugiat. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia curae; Curabitur vitae elit non nulla auctor mollis vel at elit. Morbi tincidunt nibh non ligula.</p>
+    <p className="mx-auto leading-relaxed text-base mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus in vestibulum libero. Vestibulum posuere placerat malesuada. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam at tincidunt quam. Sed mollis ligula semper sem cursus varius. Nullam a ex quis metus aliquet tempus. Ut ut ligula sit amet lectus venenatis venenatis sit amet eget lacus. Suspendisse quis odio augue. In ex enim, tincidunt ut sagittis eget, mollis non lorem. Praesent maximus ipsum sit amet velit faucibus, a dapibus dolor venenatis. Sed sit amet laoreet odio, non egestas ipsum. Mauris sodales gravida sollicitudin. Morbi purus tortor, hendrerit.</p>
+  </div>
+  )
+}
+
+export default About
---
+++ mern-blog/client/src/pages/Article.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Article = () => {
+  return (
+    <div>
+      <h1 className='sm:text-4xl text-2xl font-blod my-6 text-gray-900'>This is Article</h1>
+    </div>
+  )
+}
+
+export default Article
